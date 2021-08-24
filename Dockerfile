ff9986d048b0e0af84a7c89ebb98491730a1f504
root <root@ip-172-31-30-240.us-west-2.compute.internal>
2021-08-24 22:42:46 +0000

    version 2

--- Dockerfile
+++ Dockerfile
@@ -11,6 +11,6 @@ ADD . /app
 
 RUN pip3 install --upgrade pip
 RUN pip3 install -r requirements.txt
-EXPOSE 5000
+
 # run the command to start uWSGI
 CMD ["uwsgi", "app.ini"]
--- app/views.py
+++ app/views.py
@@ -3,11 +3,4 @@ import os
 
 @app.route("/")
 def index():
-
-    # Use os.getenv("key") to get environment variables
-    app_name = os.getenv("APP_NAME")
-
-    if app_name:
-        return "Hello from {app_name} running in a Docker container behind Nginx!"
-
-    return "Hello from Flask"
+    return "Hello World!!"
